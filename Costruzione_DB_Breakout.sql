30efdcf2e44d107104a5d369ecd82f642dc3304a
Thekrazyking90 <falcor1990@gmail.com>
2018-05-03 16:10:52 +0200

    [Costruzione_DB_Breakout.sql] aggiunta lunghezza tronchi

--- Costruzione_DB_Breakout.sql
+++ Costruzione_DB_Breakout.sql
@@ -40,6 +40,7 @@ create table "APP"."TRONCO"
 (
 	ID_tronco int primary key not null generated always as identity (start with 1, increment by 1),
 	larghezza float not null,
+        lunghezza float not null,
 	ID_mappa int references "APP"."MAPPA"(ID_mappa) on delete cascade,
         ID_nodo1 int references "APP"."NODI"(ID_nodo) on delete cascade,
         ID_nodo2 int references "APP"."NODI"(ID_nodo) on delete cascade,
